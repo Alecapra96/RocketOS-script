8a1e66d7b9e781827331616371e6e7b74d586933
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-10-25 17:49:32 -0300

    Agrego xinit y tinr2rc

--- script.sh
+++ script.sh
@@ -2,9 +2,11 @@
 
 #Actualizo el sistema
 sudo apt install && sudo apt upgrade
+
 #Instalo dependencias bases
-sudo apt -y install --no-install-suggests --no-install-recommends xserver-xorg-core software-properties-common broadcom-sta-dkms cmake libfreetype6-dev libfontconfig1-dev xclip build-essential libx11-dev libxft-dev sakura thu$git clone https://github.com/baskerville/bspwm.git
+sudo apt -y install --no-install-suggests --no-install-recommends xserver-xorg-core software-properties-common broadcom-sta-dkms cmake libfreetype6-dev libfontconfig1-dev xclip build-essential libx11-dev libxft-dev sakura build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev 
 #Instalo BSPWM
+git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
 cd bspwm/
 make
@@ -21,11 +23,19 @@ cd ../bspwm/
 cp examples/bspwmrc ~/.config/bspwm/
 chmod +x ~/.config/bspwm/bspwmrc
 cp examples/sxhkdrc ~/.config/sxhkd/
+
 #Instalo el script de startx
 sudo apt -y install xinit
+#Creo el archivo .xinitrc
+sudo mv ~/RocketOS-script/.xinitrc ~/.xinitrc
 
 
-#Agregar archivo XINIT
+
+#Instalo programas base 
+sudo apt -y install --no-install-suggests --no-install-recommends thunar screenfetch neofetch slim git lxappearance-obconf lxappearance rofi htop feh obmenu pulseaudio volti wicd tint2 libgtk-3-dev compton rdesktop  
+
+
+
 #Cambiar configuracion bspwm y sxhkd 
 #Agregar TINT2 configuracion
 #Agregar transparencia
