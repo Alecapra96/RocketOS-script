1645aae844ad0301ba1f7f15ac8b6b36714468b4
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-10-30 12:29:36 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,7 +33,7 @@ wait 50000
 sudo apt -y install xinit
 wait 50000
 #Creo el archivo .xinitrc
-sudo mv ~/RocketOS-script/.xinitrc ~/.xinitrc
+cp ~/RocketOS-script/.xinitrc ~/.xinitrc
 
 
 wait 50000
@@ -45,29 +45,31 @@ sudo apt -y purge snapd
 wait 50000
 #Cambiar configuracion bspwm y sxhkd 
     #LINA 127 dl sxhkdrc hay que poner un $hostname
-sudo mv ~/RocketOS-script/sxhkdrc ~/.config/sxhkd/
+cp ~/RocketOS-script/sxhkdrc ~/.config/sxhkd/
 
 #Creo el script del bspwm
     #Crear si no existe
 mkdir ~/.config/bspwm/scripts/
-sudo mv ~/RocketOS-script/bspwm_resize ~/.config/bspwm/scripts/
+cp ~/RocketOS-script/bspwm_resize ~/.config/bspwm/scripts/
 sudo chmod +x ~/.config/bspwm/scripts/bspwm_resize
 
 wait 50000
 #Agregar TINT2 configuracion
     #Borrar si existe
-sudo rm ~/.config/tint2/tint2rc
-sudo mv ~/RocketOS-script/tint2rc ~/.config/tint2/
+rm ~/.config/tint2/tint2rc
+cp ~/RocketOS-script/tint2rc ~/.config/tint2/
 wait 50000
 #Agregar transparencia
 #Agregar configuracion de xterm
-sudo mv ~/RocketOS-script/.Xresources ~/
+cp ~/RocketOS-script/.Xresources ~/
 xrdb -merge ~/.Xresources
 wait 50000
 #configuro Slim
 cd ~/RocketOS-script/
 git clone https://github.com/adi1090x/slim_themes
-sudo cp ~/RocketOS-script/slim_themes/themes/hello /usr/share/slim/themes
+wait 50000
+
+cp ~/RocketOS-script/slim_themes/themes/hello /usr/share/slim/themes
 wait 50000
 sudo sed -i '37s+.*+ login_cmd           exec /bin/bash - ~/.xinitrc %session +g' /etc/slim.conf
 sudo sed -i '70s+.*+default_user       v4ntush+g' /etc/slim.conf
